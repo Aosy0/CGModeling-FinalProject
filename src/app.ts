32de6a1970cefc9ee914b72c3d6b9c7141e18918
Aosy <koboyutaao@gmail.com>
2025-07-17 14:42:32 +0900

    VSCodeのGUIから最初のコミット

---
+++ src/app.ts
@@ -0,0 +1,97 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+class ThreeJSContainer {
+    private scene: THREE.Scene;
+    private geometry: THREE.BufferGeometry;
+    private material: THREE.Material;
+    private cube: THREE.Mesh;
+    private light: THREE.Light;
+
+    constructor() {
+
+    }
+
+    // 画面部分の作成(表示する枠ごとに)*
+    public createRendererDOM = (width: number, height: number, cameraPos: THREE.Vector3) => {
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize(width, height);
+        renderer.setClearColor(new THREE.Color(0x495ed));
+        renderer.shadowMap.enabled = true; //シャドウマップを有効にする
+
+        //カメラの設定
+        let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        camera.position.copy(cameraPos);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        let orbitControls = new OrbitControls(camera, renderer.domElement);
+
+        this.createScene();
+        // 毎フレームのupdateを呼んで，render
+        // reqestAnimationFrame により次フレームを呼ぶ
+        let render: FrameRequestCallback = (time) => {
+            orbitControls.update();
+
+            renderer.render(this.scene, camera);
+            requestAnimationFrame(render);
+        }
+        requestAnimationFrame(render);
+
+        renderer.domElement.style.cssFloat = "left";
+        renderer.domElement.style.margin = "10px";
+        return renderer.domElement;
+    }
+
+    // シーンの作成(全体で1回)
+    private createScene = () => {
+        this.scene = new THREE.Scene();
+
+        //平面の関数
+        let myPlane = (u: number, v: number, target: THREE.Vector3) => {
+            let r = 30;
+            let x = u * r - r / 2;
+            let y = 0;
+            let z = v * r - r / 2;
+            target.set(x, y, z);
+        }
+
+        //ジオメトリの作成
+        let paramGeometry = new THREE.ParametricGeometry(myPlane, 10, 10);
+
+        let paramMaterial = new THREE.MeshPhongMaterial({ color: 0x00ffff, side: THREE.DoubleSide, flatShading: true });
+        //ワイヤーフレーム
+        let lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.5 });
+
+        let group = new THREE.Group();
+        group.add(new THREE.Mesh(paramGeometry, paramMaterial));
+        group.add(new THREE.LineSegments(paramGeometry, lineMaterial));
+
+        this.scene.add(group);
+
+
+
+        //ライトの設定
+        this.light = new THREE.DirectionalLight(0xffffff);
+        let lvec = new THREE.Vector3(1, 1, 1).normalize();
+        this.light.position.set(lvec.x, lvec.y, lvec.z);
+        this.scene.add(this.light);
+
+        // 毎フレームのupdateを呼んで，更新
+        // reqestAnimationFrame により次フレームを呼ぶ
+        let update: FrameRequestCallback = (time) => {
+            this.cube.rotateX(0.01);
+
+            requestAnimationFrame(update);
+        }
+        requestAnimationFrame(update);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    let container = new ThreeJSContainer();
+
+    let viewport = container.createRendererDOM(640, 480, new THREE.Vector3(-3, 3, 3));
+    document.body.appendChild(viewport);
+}
